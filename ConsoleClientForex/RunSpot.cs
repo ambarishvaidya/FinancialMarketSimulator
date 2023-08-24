3c2838ba5899cdb8b925252238d74bd8021a5be6
Ambarish Vaidya <ambarish.vaidya@gmail.com>
2023-08-24 11:43:47 +0100

    Changes to Console to test Adding new tick at runtime.

--- ConsoleClientForex/RunSpot.cs
+++ ConsoleClientForex/RunSpot.cs
@@ -10,10 +10,12 @@ namespace ConsoleClientForex
         private string _testFilePath;
         private const string TestForexSpotFileName = "Forex.Spot.TestFile.txt";
         private readonly ISpot spot;
-        private readonly System.Timers.Timer _pauseTimer, _resumeTimer, _stopTimer;
-        private readonly int PAUSE_TIME = 5000;
+        private readonly System.Timers.Timer _pauseTimer, _resumeTimer, _stopTimer, _addNewPair;
+
+        private readonly int ADDNEWPAIR_TIME = 15000;
+        private readonly int PAUSE_TIME = 20000;
         private readonly int RESUME_TIME = 10000;
-        private readonly int STOP_TIME = 5000;
+        private readonly int STOP_TIME = 5000;        
 
         private bool _createFromFile = true;
 
@@ -25,7 +27,10 @@ namespace ConsoleClientForex
             
             spot = _createFromFile ? CreateSpotFromFile(loggerFactory) : CreateSpotFromTickerDefinition(loggerFactory);
             
-            //spot.OnTickUpdate += Spot_OnTickUpdate;
+            spot.OnTickUpdate += Spot_OnTickUpdate;
+
+            _addNewPair = new System.Timers.Timer(ADDNEWPAIR_TIME);
+            _addNewPair.Elapsed += AddNewPair_Elapsed;
 
             _pauseTimer = new System.Timers.Timer(PAUSE_TIME);
             _pauseTimer.Elapsed += PauseTimer_Elapsed;
@@ -38,8 +43,18 @@ namespace ConsoleClientForex
             _stopTimer.Elapsed += StopTimer_Elapsed;
 
             spot.Start();
+            _addNewPair.Start();
             if(File.Exists(_testFilePath))
                 File.Delete(_testFilePath);
+        }
+
+        private void AddNewPair_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _addNewPair.Stop();
+            _logger.LogInformation("Adding new Ccy Pair ABCPQR !");
+            spot.AddTickDefinition(new TickDefinition("ABCPQR", 123.456, 123.478, 0.55, 1000));
+            //spot.Stop();
+            //spot.Start();
         }
 
         private void StopTimer_Elapsed(object sender, ElapsedEventArgs e)
@@ -95,22 +110,22 @@ namespace ConsoleClientForex
             {
                 sw.WriteLine("CurrencyPair,Bid,Ask,Spread,PublishFrequencyInMs");
                 sw.WriteLine("EURUSD, 1.1234, 1.1235, 1.1236, 1500");
-                sw.WriteLine("EURGBP1, 0.8901, 0.8902, 0.8903, 123");
-                sw.WriteLine("EURGBP2, 0.8901, 0.8902, 0.8903, 123");
-                sw.WriteLine("EURGBP3, 0.8901, 0.8902, 0.8903, 123");
-                sw.WriteLine("EURGBP4, 0.8901, 0.8902, 0.8903, 123");
-                sw.WriteLine("EURGBP5, 0.8901, 0.8902, 0.8903, 123");
-                sw.WriteLine("EURGBP6, 0.8901, 0.8902, 0.8903, 123");
-                sw.WriteLine("EURGBP7, 0.8901, 0.8902, 0.8903, 123");
-                sw.WriteLine("EURGBP8, 0.8901, 0.8902, 0.8903, 123");
-                sw.WriteLine("EURGBP9, 0.8901, 0.8902, 0.8903, 123");
-                sw.WriteLine("EURGBP06, 0.8901, 0.8902, 0.8903, 1230");
-                sw.WriteLine("EURGBP03, 0.8901, 0.8902, 0.8903, 1230");
-                sw.WriteLine("EURGBP03, 0.8901, 0.8902, 0.8903, 1230");
-                sw.WriteLine("EURGBP03, 0.8901, 0.8902, 0.8903, 1230");
-                sw.WriteLine("EURGBP04, 0.8901, 0.8902, 0.8903, 1230");
-                sw.WriteLine("EURGBP05, 0.8901, 0.8902, 0.8903, 1230");
-                sw.WriteLine("EURGBP06, 0.8901, 0.8902, 0.8903, 1230");
+                //sw.WriteLine("EURGBP1, 0.8901, 0.8902, 0.8903, 123");
+                //sw.WriteLine("EURGBP2, 0.8901, 0.8902, 0.8903, 123");
+                //sw.WriteLine("EURGBP3, 0.8901, 0.8902, 0.8903, 123");
+                //sw.WriteLine("EURGBP4, 0.8901, 0.8902, 0.8903, 123");
+                //sw.WriteLine("EURGBP5, 0.8901, 0.8902, 0.8903, 123");
+                //sw.WriteLine("EURGBP6, 0.8901, 0.8902, 0.8903, 123");
+                //sw.WriteLine("EURGBP7, 0.8901, 0.8902, 0.8903, 123");
+                //sw.WriteLine("EURGBP8, 0.8901, 0.8902, 0.8903, 123");
+                //sw.WriteLine("EURGBP9, 0.8901, 0.8902, 0.8903, 123");
+                //sw.WriteLine("EURGBP06, 0.8901, 0.8902, 0.8903, 1230");
+                //sw.WriteLine("EURGBP03, 0.8901, 0.8902, 0.8903, 1230");
+                //sw.WriteLine("EURGBP03, 0.8901, 0.8902, 0.8903, 1230");
+                //sw.WriteLine("EURGBP03, 0.8901, 0.8902, 0.8903, 1230");
+                //sw.WriteLine("EURGBP04, 0.8901, 0.8902, 0.8903, 1230");
+                //sw.WriteLine("EURGBP05, 0.8901, 0.8902, 0.8903, 1230");
+                //sw.WriteLine("EURGBP06, 0.8901, 0.8902, 0.8903, 1230");
 
 
             }
